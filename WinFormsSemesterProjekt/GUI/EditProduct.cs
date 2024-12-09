683234ab9e2024b46bb942642e85614fc7ceb87b
Sarah <peasblinky@gmail.com>
2024-12-09 14:50:46 +0100

    Added EditProducts.cs (v1.0)

---
+++ WinFormsSemesterProjekt/GUI/EditProduct.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsSemesterProjekt.GUI
+{
+    public partial class EditProduct : Form
+    {
+        public EditProduct()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonReturn_Click(object sender, EventArgs e)
+        {
+            Products products = new Products();
+            products.Show();
+            this.Hide();
+        }
+    }
+}
