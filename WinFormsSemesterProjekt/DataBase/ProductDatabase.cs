6a6ecb57b22cec649694b872c9ec8c40e6bf45e4
MikkelSchmidtNielsen <mikkelschmidt@msn.com>
2024-12-09 12:14:14 +0100

    Created new branch Added ProductDatabase Added CreateNewProduct under ProductDatabase Updated Product model

---
+++ WinFormsSemesterProjekt/DataBase/ProductDatabase.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WinformsSemesterprojekt.Models;
+
+namespace WinFormsSemesterProjekt.DataBase
+{
+	internal class ProductDatabase : DatabaseManager
+	{
+		public int CreateNewProduct(Product product)
+		{
+			string query =
+				"insert into Product (Name, Category, Desription, Price, Stock) " +
+				"values (@Name, @Category, @Desription, @Price, @Stock)";
+
+			var command = new SqlCommand(query, connection);
+
+			command.Parameters.AddWithValue("@Name", product.ProductName);
+			command.Parameters.AddWithValue("@Category", product.Category);
+			command.Parameters.AddWithValue("@Desription", product.Description);
+			command.Parameters.AddWithValue("@Price", product.UnitPrice);
+			command.Parameters.AddWithValue("@Stock", product.Stock);
+
+			int productId = DatabaseManager.ExecuteScalar(command);
+
+			return productId;
+		}
+	}
+}
--- WinFormsSemesterProjekt/Models/Product.cs
+++ WinFormsSemesterProjekt/Models/Product.cs
@@ -10,52 +10,19 @@ namespace WinformsSemesterprojekt.Models
     {
         public int ProductID { get; private set; }
         public string ProductName { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
         public double UnitPrice { get; set; }
-        public int InStock { get; set; }
+        public int Stock { get; set; }
 
-        public Product(int productID, string productName, int unitPrice, int inStock)
-        {
-            ProductID = productID;
-            ProductName = productName;
-            UnitPrice = unitPrice;
-            InStock = inStock;
-        }
-
-        public void UpdatePrice(string input)
-        {
-            Methods methods = new Methods();
-
-            double newPrice;
-            bool result = false;
-
-            result = double.TryParse(input, out newPrice);
-
-            while (result == false)
-            {
-                methods.PrintText("Please enter a valid number");
-                input = Console.ReadLine();
-                result = double.TryParse(input, out newPrice);
-            }
-
-            UnitPrice = newPrice;
-        }
-
-        public void UpdateStock(string input)
-        {
-            Methods methods = new Methods();
-
-            int newStock;
-            bool result = false;
-
-            result = int.TryParse(input, out newStock);
-
-            while (result == false)
-            {
-                methods.PrintText("Please enter a valid number");
-                input = Console.ReadLine();
-                result = int.TryParse(input, out newStock);
-            }
-            InStock = newStock;
-        }
-    }
+		public Product(int productID, string productName, string category, string description, double unitPrice, int stock)
+		{
+			ProductID = productID;
+			ProductName = productName;
+			Category = category;
+			Description = description;
+			UnitPrice = unitPrice;
+			Stock = stock;
+		}
+	}
 }
