1faffbd63a3add80759e1c8ccbba7e5899d6c612
LuffeDeluXe <luffedeluxe@gmail.com>
2024-12-11 14:10:38 +0100

    Fixed Customer

--- WinFormsSemesterProjekt/GUI/MainMenu.cs
+++ WinFormsSemesterProjekt/GUI/MainMenu.cs
@@ -13,5 +13,12 @@ namespace WinFormsSemesterProjekt
             products.Show();
             this.Hide();
         }
+
+        private void buttonOrders_Click(object sender, EventArgs e)
+        {
+            Orders orders = new Orders();
+            orders.Show();
+            this.Hide();
+        }
     }
 }
