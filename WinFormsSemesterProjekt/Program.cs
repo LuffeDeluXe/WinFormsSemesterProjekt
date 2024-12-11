72ce378b5648f494da737d58b942fcb13fab8c8d
LuffeDeluXe <luffedeluxe@gmail.com>
2024-12-11 14:11:01 +0100

    Fixed ProgramCS

--- WinFormsSemesterProjekt/Program.cs
+++ WinFormsSemesterProjekt/Program.cs
@@ -14,10 +14,13 @@ namespace WinFormsSemesterProjekt
             var method = new CustomerDatabase();
             var anders = new Customer("Anders", "Jensen", 25252525, "aj@pp.dk");
             CustomerDatabase.UpdateCustomerInformation(anders);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainMenu());
             //anders.UpdateEmail("123");
-            
+
             //method.CreateCustomer(anders);
-    
+
 
         }
     }
