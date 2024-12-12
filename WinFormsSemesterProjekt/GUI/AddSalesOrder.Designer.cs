99d43a3cf8dc6b363dd5cc904af0966535e678dd
LuffeDeluXe <luffedeluxe@gmail.com>
2024-12-12 11:05:12 +0100

    Changed OrderWindow

--- WinFormsSemesterProjekt/GUI/AddSalesOrder.Designer.cs
+++ WinFormsSemesterProjekt/GUI/AddSalesOrder.Designer.cs
@@ -42,9 +42,10 @@
             // 
             // buttonReturn
             // 
-            buttonReturn.Location = new Point(22, 409);
+            buttonReturn.Location = new Point(19, 307);
+            buttonReturn.Margin = new Padding(3, 2, 3, 2);
             buttonReturn.Name = "buttonReturn";
-            buttonReturn.Size = new Size(158, 29);
+            buttonReturn.Size = new Size(138, 22);
             buttonReturn.TabIndex = 26;
             buttonReturn.Text = "Tilbage til oversigt";
             buttonReturn.UseVisualStyleBackColor = true;
@@ -52,9 +53,10 @@
             // 
             // buttonAdd
             // 
-            buttonAdd.Location = new Point(369, 409);
+            buttonAdd.Location = new Point(323, 307);
+            buttonAdd.Margin = new Padding(3, 2, 3, 2);
             buttonAdd.Name = "buttonAdd";
-            buttonAdd.Size = new Size(93, 29);
+            buttonAdd.Size = new Size(81, 22);
             buttonAdd.TabIndex = 25;
             buttonAdd.Text = "Sælg";
             buttonAdd.UseVisualStyleBackColor = true;
@@ -64,51 +66,55 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Trebuchet MS", 28.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label1.Location = new Point(221, 9);
+            label1.Location = new Point(193, 7);
             label1.Name = "label1";
-            label1.Size = new Size(374, 59);
+            label1.Size = new Size(303, 49);
             label1.TabIndex = 14;
             label1.Text = "Opret salgsordre";
             // 
             // listBoxProducts
             // 
             listBoxProducts.FormattingEnabled = true;
-            listBoxProducts.Location = new Point(306, 124);
+            listBoxProducts.ItemHeight = 15;
+            listBoxProducts.Location = new Point(268, 93);
+            listBoxProducts.Margin = new Padding(3, 2, 3, 2);
             listBoxProducts.Name = "listBoxProducts";
-            listBoxProducts.Size = new Size(236, 104);
+            listBoxProducts.Size = new Size(207, 79);
             listBoxProducts.TabIndex = 27;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(224, 124);
+            label2.Location = new Point(196, 93);
             label2.Name = "label2";
-            label2.Size = new Size(76, 20);
+            label2.Size = new Size(62, 15);
             label2.TabIndex = 28;
             label2.Text = "Produkter:";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(126, 272);
+            label3.Location = new Point(200, 204);
             label3.Name = "label3";
-            label3.Size = new Size(174, 20);
+            label3.Size = new Size(58, 15);
             label3.TabIndex = 29;
-            label3.Text = "Kundens telefonnummer:";
+            label3.Text = "Kunde ID:";
             // 
             // textBoxCustomerPhoneNumber
             // 
             textBoxCustomerPhoneNumber.BorderStyle = BorderStyle.FixedSingle;
-            textBoxCustomerPhoneNumber.Location = new Point(306, 270);
+            textBoxCustomerPhoneNumber.Location = new Point(268, 202);
+            textBoxCustomerPhoneNumber.Margin = new Padding(3, 2, 3, 2);
             textBoxCustomerPhoneNumber.Name = "textBoxCustomerPhoneNumber";
-            textBoxCustomerPhoneNumber.Size = new Size(236, 27);
+            textBoxCustomerPhoneNumber.Size = new Size(207, 23);
             textBoxCustomerPhoneNumber.TabIndex = 30;
             // 
             // buttonAddProducts
             // 
-            buttonAddProducts.Location = new Point(548, 199);
+            buttonAddProducts.Location = new Point(480, 149);
+            buttonAddProducts.Margin = new Padding(3, 2, 3, 2);
             buttonAddProducts.Name = "buttonAddProducts";
-            buttonAddProducts.Size = new Size(132, 29);
+            buttonAddProducts.Size = new Size(116, 22);
             buttonAddProducts.TabIndex = 31;
             buttonAddProducts.Text = "Tilføj produkter";
             buttonAddProducts.UseVisualStyleBackColor = true;
@@ -116,26 +122,27 @@
             // textBox1
             // 
             textBox1.BorderStyle = BorderStyle.FixedSingle;
-            textBox1.Location = new Point(306, 234);
+            textBox1.Location = new Point(268, 176);
+            textBox1.Margin = new Padding(3, 2, 3, 2);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(236, 27);
+            textBox1.Size = new Size(207, 23);
             textBox1.TabIndex = 32;
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(253, 236);
+            label4.Location = new Point(220, 178);
             label4.Name = "label4";
-            label4.Size = new Size(47, 20);
+            label4.Size = new Size(38, 15);
             label4.TabIndex = 33;
             label4.Text = "Antal:";
             // 
             // AddSalesOrder
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.LightGoldenrodYellow;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(700, 338);
             Controls.Add(label4);
             Controls.Add(textBox1);
             Controls.Add(buttonAddProducts);
@@ -146,6 +153,7 @@
             Controls.Add(buttonReturn);
             Controls.Add(buttonAdd);
             Controls.Add(label1);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "AddSalesOrder";
             Text = "AddSalesOrder";
             ResumeLayout(false);
