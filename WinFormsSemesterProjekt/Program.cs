b1f6a7f3c08e99967011b3d3b1835f0e404f1f37
MikkelRLarsen <Thenuker95@gmail.com>
2024-12-16 09:08:55 +0100

    Fixed some bugs

--- WinFormsSemesterProjekt/Program.cs
+++ WinFormsSemesterProjekt/Program.cs
@@ -10,12 +10,7 @@ namespace WinFormsSemesterProjekt
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
-        public static void Initialize()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
-        }
+
         static void Main()
         {
             Initialize();
