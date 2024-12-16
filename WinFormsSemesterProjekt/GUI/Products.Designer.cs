d1d10b3f8d7e03e52f6ecb0adbd81a1d0ce693b0
MikkelRLarsen <Thenuker95@gmail.com>
2024-12-16 10:50:45 +0100

    Fixed a bug with Export Feature

--- WinFormsSemesterProjekt/GUI/Products.Designer.cs
+++ WinFormsSemesterProjekt/GUI/Products.Designer.cs
@@ -20,7 +20,7 @@
             base.Dispose(disposing);
         }
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
         /// <summary>
         /// Required method for Designer support - do not modify
@@ -50,9 +50,10 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Trebuchet MS", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label1.Location = new Point(12, 9);
+            label1.Location = new Point(20, 14);
+            label1.Margin = new Padding(5, 0, 5, 0);
             label1.Name = "label1";
-            label1.Size = new Size(197, 49);
+            label1.Size = new Size(315, 81);
             label1.TabIndex = 0;
             label1.Text = "Produkter";
             // 
@@ -60,9 +61,10 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Trebuchet MS", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label2.Location = new Point(12, 91);
+            label2.Location = new Point(20, 146);
+            label2.Margin = new Padding(5, 0, 5, 0);
             label2.Name = "label2";
-            label2.Size = new Size(202, 26);
+            label2.Size = new Size(314, 40);
             label2.TabIndex = 1;
             label2.Text = "Filtrer efter kategori";
             // 
@@ -70,9 +72,10 @@
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Trebuchet MS", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label3.Location = new Point(12, 284);
+            label3.Location = new Point(20, 454);
+            label3.Margin = new Padding(5, 0, 5, 0);
             label3.Name = "label3";
-            label3.Size = new Size(227, 26);
+            label3.Size = new Size(354, 40);
             label3.TabIndex = 2;
             label3.Text = "Filtrer efter lagerstatus";
             // 
@@ -81,9 +84,10 @@
             buttonSearch.BackColor = Color.Black;
             buttonSearch.FlatStyle = FlatStyle.Flat;
             buttonSearch.ForeColor = SystemColors.ControlLightLight;
-            buttonSearch.Location = new Point(694, 22);
+            buttonSearch.Location = new Point(1128, 35);
+            buttonSearch.Margin = new Padding(5, 5, 5, 5);
             buttonSearch.Name = "buttonSearch";
-            buttonSearch.Size = new Size(94, 29);
+            buttonSearch.Size = new Size(153, 46);
             buttonSearch.TabIndex = 3;
             buttonSearch.Text = "Søg";
             buttonSearch.UseVisualStyleBackColor = false;
@@ -93,9 +97,10 @@
             buttonMainMenu.BackColor = Color.Black;
             buttonMainMenu.FlatStyle = FlatStyle.Flat;
             buttonMainMenu.ForeColor = SystemColors.ControlLightLight;
-            buttonMainMenu.Location = new Point(12, 409);
+            buttonMainMenu.Location = new Point(20, 654);
+            buttonMainMenu.Margin = new Padding(5, 5, 5, 5);
             buttonMainMenu.Name = "buttonMainMenu";
-            buttonMainMenu.Size = new Size(123, 29);
+            buttonMainMenu.Size = new Size(200, 46);
             buttonMainMenu.TabIndex = 4;
             buttonMainMenu.Text = "Hovedmenu";
             buttonMainMenu.UseVisualStyleBackColor = false;
@@ -104,9 +109,10 @@
             // searchBar
             // 
             searchBar.BorderStyle = BorderStyle.FixedSingle;
-            searchBar.Location = new Point(313, 24);
+            searchBar.Location = new Point(509, 38);
+            searchBar.Margin = new Padding(5, 5, 5, 5);
             searchBar.Name = "searchBar";
-            searchBar.Size = new Size(375, 27);
+            searchBar.Size = new Size(608, 39);
             searchBar.TabIndex = 5;
             searchBar.Enter += searchBar_Enter;
             searchBar.Leave += searchBar_Leave;
@@ -114,10 +120,11 @@
             // dataGridView1
             // 
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(313, 91);
+            dataGridView1.Location = new Point(509, 146);
+            dataGridView1.Margin = new Padding(5, 5, 5, 5);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 51;
-            dataGridView1.Size = new Size(475, 304);
+            dataGridView1.Size = new Size(772, 486);
             dataGridView1.TabIndex = 6;
             // 
             // buttonNewProduct
@@ -125,9 +132,10 @@
             buttonNewProduct.BackColor = Color.Black;
             buttonNewProduct.FlatStyle = FlatStyle.Flat;
             buttonNewProduct.ForeColor = SystemColors.ControlLightLight;
-            buttonNewProduct.Location = new Point(313, 409);
+            buttonNewProduct.Location = new Point(509, 654);
+            buttonNewProduct.Margin = new Padding(5, 5, 5, 5);
             buttonNewProduct.Name = "buttonNewProduct";
-            buttonNewProduct.Size = new Size(148, 29);
+            buttonNewProduct.Size = new Size(240, 46);
             buttonNewProduct.TabIndex = 7;
             buttonNewProduct.Text = "Nyt produkt";
             buttonNewProduct.UseVisualStyleBackColor = false;
@@ -138,20 +146,23 @@
             buttonEditProduct.BackColor = Color.Black;
             buttonEditProduct.FlatStyle = FlatStyle.Flat;
             buttonEditProduct.ForeColor = SystemColors.ControlLightLight;
-            buttonEditProduct.Location = new Point(476, 409);
+            buttonEditProduct.Location = new Point(774, 654);
+            buttonEditProduct.Margin = new Padding(5, 5, 5, 5);
             buttonEditProduct.Name = "buttonEditProduct";
-            buttonEditProduct.Size = new Size(148, 29);
+            buttonEditProduct.Size = new Size(240, 46);
             buttonEditProduct.TabIndex = 8;
             buttonEditProduct.Text = "Ændr valgt produkt";
             buttonEditProduct.UseVisualStyleBackColor = false;
+            buttonEditProduct.Click += buttonEditProduct_Click;
             // 
             // buttonDeleteProduct
             // 
             buttonDeleteProduct.BackColor = Color.Black;
             buttonDeleteProduct.ForeColor = SystemColors.ControlLightLight;
-            buttonDeleteProduct.Location = new Point(640, 409);
+            buttonDeleteProduct.Location = new Point(1040, 654);
+            buttonDeleteProduct.Margin = new Padding(5, 5, 5, 5);
             buttonDeleteProduct.Name = "buttonDeleteProduct";
-            buttonDeleteProduct.Size = new Size(148, 29);
+            buttonDeleteProduct.Size = new Size(240, 46);
             buttonDeleteProduct.TabIndex = 9;
             buttonDeleteProduct.Text = "Slet valgt produkt";
             buttonDeleteProduct.UseVisualStyleBackColor = false;
@@ -160,17 +171,19 @@
             // listBoxCategories
             // 
             listBoxCategories.FormattingEnabled = true;
-            listBoxCategories.Location = new Point(20, 117);
+            listBoxCategories.Location = new Point(32, 187);
+            listBoxCategories.Margin = new Padding(5, 5, 5, 5);
             listBoxCategories.Name = "listBoxCategories";
-            listBoxCategories.Size = new Size(222, 164);
+            listBoxCategories.Size = new Size(358, 260);
             listBoxCategories.TabIndex = 11;
             // 
             // radioButtonAvailable
             // 
             radioButtonAvailable.AutoSize = true;
-            radioButtonAvailable.Location = new Point(20, 313);
+            radioButtonAvailable.Location = new Point(32, 501);
+            radioButtonAvailable.Margin = new Padding(5, 5, 5, 5);
             radioButtonAvailable.Name = "radioButtonAvailable";
-            radioButtonAvailable.Size = new Size(83, 24);
+            radioButtonAvailable.Size = new Size(129, 36);
             radioButtonAvailable.TabIndex = 12;
             radioButtonAvailable.TabStop = true;
             radioButtonAvailable.Text = "På lager";
@@ -179,9 +192,10 @@
             // radioButtonUnavailable
             // 
             radioButtonUnavailable.AutoSize = true;
-            radioButtonUnavailable.Location = new Point(20, 343);
+            radioButtonUnavailable.Location = new Point(32, 549);
+            radioButtonUnavailable.Margin = new Padding(5, 5, 5, 5);
             radioButtonUnavailable.Name = "radioButtonUnavailable";
-            radioButtonUnavailable.Size = new Size(115, 24);
+            radioButtonUnavailable.Size = new Size(181, 36);
             radioButtonUnavailable.TabIndex = 13;
             radioButtonUnavailable.TabStop = true;
             radioButtonUnavailable.Text = "Ikke på lager";
@@ -192,12 +206,14 @@
             buttonExport.BackColor = Color.Black;
             buttonExport.FlatStyle = FlatStyle.Flat;
             buttonExport.ForeColor = SystemColors.ControlLightLight;
-            buttonExport.Location = new Point(313, 57);
+            buttonExport.Location = new Point(509, 91);
+            buttonExport.Margin = new Padding(5, 5, 5, 5);
             buttonExport.Name = "buttonExport";
-            buttonExport.Size = new Size(204, 29);
+            buttonExport.Size = new Size(332, 46);
             buttonExport.TabIndex = 14;
             buttonExport.Text = "Eksporter lagerstatus til .txt";
             buttonExport.UseVisualStyleBackColor = false;
+            buttonExport.Click += buttonExport_Click;
             // 
             // comboBoxSortby
             // 
@@ -206,18 +222,19 @@
             comboBoxSortby.ForeColor = SystemColors.ControlLightLight;
             comboBoxSortby.FormattingEnabled = true;
             comboBoxSortby.Items.AddRange(new object[] { "Navn: A-Å", "Navn: Å-A", "Kategori: A-Å", "Kategori: Å-A", "Pris: Stigende", "Pris: Faldende" });
-            comboBoxSortby.Location = new Point(670, 58);
+            comboBoxSortby.Location = new Point(1089, 93);
+            comboBoxSortby.Margin = new Padding(5, 5, 5, 5);
             comboBoxSortby.Name = "comboBoxSortby";
-            comboBoxSortby.Size = new Size(118, 28);
+            comboBoxSortby.Size = new Size(189, 40);
             comboBoxSortby.TabIndex = 47;
             comboBoxSortby.Text = "Sorter efter";
             // 
             // Products
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.LightGoldenrodYellow;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1300, 720);
             Controls.Add(comboBoxSortby);
             Controls.Add(buttonExport);
             Controls.Add(radioButtonUnavailable);
@@ -233,6 +250,7 @@
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
+            Margin = new Padding(5, 5, 5, 5);
             Name = "Products";
             Text = "Products";
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
@@ -240,9 +258,9 @@
             PerformLayout();
         }
 
-		#endregion
+        #endregion
 
-		private Label label1;
+        private Label label1;
         private Label label2;
         private Label label3;
         private Button buttonSearch;
--- WinFormsSemesterProjekt/GUI/Products.cs
+++ WinFormsSemesterProjekt/GUI/Products.cs
@@ -19,198 +19,198 @@ using WinFormsSemesterProjekt.Models;
 
 namespace WinFormsSemesterProjekt
 {
-	public partial class Products : Form
-	{
-		private List<string> _Categories { get; set; }
+    public partial class Products : Form
+    {
+        private List<string> _Categories { get; set; }
 
-		public static Panel ProductsPanel;
+        public static Panel ProductsPanel;
 
-		private BindingList<Product> ProductList { get; set; }
+        private BindingList<Product> ProductList { get; set; }
 
-		private BindingList<Product> FilteredList { get; set; }
+        private BindingList<Product> FilteredList { get; set; }
 
-		private int ProductID { get; set; }
+        private int ProductID { get; set; }
 
-		public Products()
-		{
-			InitializeComponent();
+        public Products()
+        {
+            InitializeComponent();
 
-			ProductList = new BindingList<Product>(ProductDatabase.FindAllProducts());
-			FilteredList = new BindingList<Product>();
-			dataGridView1.DataSource = ProductList;
-			searchBar.Text = "Søg efter et produkt...";
-			searchBar.ForeColor = Color.LightGray;
+            ProductList = new BindingList<Product>(ProductDatabase.FindAllProducts());
+            FilteredList = new BindingList<Product>();
+            dataGridView1.DataSource = ProductList;
+            searchBar.Text = "Søg efter et produkt...";
+            searchBar.ForeColor = Color.LightGray;
 
-			_Categories = CategoryHelpingMethod.FilterProductsByCategory(ProductList);
-			listBoxCategories.DataSource = _Categories;
-		}
+            _Categories = CategoryHelpingMethod.FilterProductsByCategory(ProductList);
+            listBoxCategories.DataSource = _Categories;
+        }
 
-		private void searchBar_Leave(object sender, EventArgs e)
-		{
-			if (searchBar.Text == "" || String.IsNullOrEmpty(searchBar.Text))
-			{
-				searchBar.Text = "Søg efter et produkt...";
-				searchBar.ForeColor = Color.LightGray;
+        private void searchBar_Leave(object sender, EventArgs e)
+        {
+            if (searchBar.Text == "" || String.IsNullOrEmpty(searchBar.Text))
+            {
+                searchBar.Text = "Søg efter et produkt...";
+                searchBar.ForeColor = Color.LightGray;
 
-				dataGridView1.DataSource = ProductList;
+                dataGridView1.DataSource = ProductList;
 
-				dataGridView1.Refresh();
-			}
-		}
+                dataGridView1.Refresh();
+            }
+        }
 
-		private void searchBar_Enter(object sender, EventArgs e)
-		{
-			if (searchBar.Text == "Søg efter et produkt..." || String.IsNullOrEmpty(searchBar.Text))
-			{
-				searchBar.Text = "";
-				searchBar.ForeColor = Color.Black;
+        private void searchBar_Enter(object sender, EventArgs e)
+        {
+            if (searchBar.Text == "Søg efter et produkt..." || String.IsNullOrEmpty(searchBar.Text))
+            {
+                searchBar.Text = "";
+                searchBar.ForeColor = Color.Black;
 
-				dataGridView1.DataSource = ProductList;
+                dataGridView1.DataSource = ProductList;
 
-				dataGridView1.Refresh();
-			}
-		}
+                dataGridView1.Refresh();
+            }
+        }
 
-		private void buttonNewProduct_Click(object sender, EventArgs e)
-		{
-			AddProduct addProduct = new AddProduct();
-			addProduct.Show();
-			this.Hide();
-		}
+        private void buttonNewProduct_Click(object sender, EventArgs e)
+        {
+            AddProduct addProduct = new AddProduct();
+            addProduct.Show();
+            this.Hide();
+        }
 
-		private void buttonDeleteProduct_Click(object sender, EventArgs e)
-		{
-			if (ProductID != 0)
-			{
-				ConfirmDeletionProduct confirmDeletionProduct = new ConfirmDeletionProduct(ProductID);
-				confirmDeletionProduct.Show();
-				this.Close();
-			}
-			else
-			{
-				MessageBox.Show("Please pick a product");
-			}
-		}
+        private void buttonDeleteProduct_Click(object sender, EventArgs e)
+        {
+            if (ProductID != 0)
+            {
+                ConfirmDeletionProduct confirmDeletionProduct = new ConfirmDeletionProduct(ProductID);
+                confirmDeletionProduct.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Please pick a product");
+            }
+        }
 
-		private void buttonSearch_Click(object sender, EventArgs e)
-		{
-			string searchInput = searchBar.Text;
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            string searchInput = searchBar.Text;
 
-			if (int.TryParse(searchInput, out int convertedInput))
-			{
-				Product foundProduct = ProductDatabase.FindProduct(convertedInput);
+            if (int.TryParse(searchInput, out int convertedInput))
+            {
+                Product foundProduct = ProductDatabase.FindProduct(convertedInput);
 
-				FilteredList.Clear();
+                FilteredList.Clear();
 
-				FilteredList.Add(foundProduct);
+                FilteredList.Add(foundProduct);
 
-				dataGridView1.DataSource = FilteredList;
+                dataGridView1.DataSource = FilteredList;
 
-				dataGridView1.Refresh();
-			}
-		}
+                dataGridView1.Refresh();
+            }
+        }
 
-		private void buttonEditProduct_Click(object sender, EventArgs e)
-		{
-			if (Name != null)
-			{
-				EditProduct editProduct = new EditProduct(ProductID);
-				editProduct.Show();
-				this.Close();
-			}
-			else
-			{
-				MessageBox.Show("Please pick a product");
-			}
-		}
+        private void buttonEditProduct_Click(object sender, EventArgs e)
+        {
+            if (Name != null)
+            {
+                EditProduct editProduct = new EditProduct(ProductID);
+                editProduct.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Please pick a product");
+            }
+        }
 
-		private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
-		{
-			DataGridViewRow selectedRow = dataGridView1.Rows[e.RowIndex];
-			ProductID = Convert.ToInt32(selectedRow.Cells[0].Value);
-		}
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewRow selectedRow = dataGridView1.Rows[e.RowIndex];
+            ProductID = Convert.ToInt32(selectedRow.Cells[0].Value);
+        }
 
-		private void listBoxCategories_SelectedIndexChanged(object sender, EventArgs e)
-		{
-			if (listBoxCategories.SelectedIndex != 0)
-			{
-				BindingList<Product> catogoryResultsDisplayed = CategoryHelpingMethod.SelectedCategoryInstances(ProductList, _Categories[listBoxCategories.SelectedIndex]);
-				dataGridView1.DataSource = catogoryResultsDisplayed;
-				dataGridView1.Refresh();
-			}
-			else
-			{
-				dataGridView1.DataSource = ProductList;
-				dataGridView1.Refresh();
-			}
-		}
+        private void listBoxCategories_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBoxCategories.SelectedIndex != 0)
+            {
+                BindingList<Product> catogoryResultsDisplayed = CategoryHelpingMethod.SelectedCategoryInstances(ProductList, _Categories[listBoxCategories.SelectedIndex]);
+                dataGridView1.DataSource = catogoryResultsDisplayed;
+                dataGridView1.Refresh();
+            }
+            else
+            {
+                dataGridView1.DataSource = ProductList;
+                dataGridView1.Refresh();
+            }
+        }
 
-		private void buttonExport_Click(object sender, EventArgs e)
-		{
-			int index = listBoxCategories.SelectedIndex;
-			WriteToTxt(listBoxCategoriesSelectedList(index));
-		}
+        private void buttonExport_Click(object sender, EventArgs e)
+        {
+            int index = listBoxCategories.SelectedIndex;
+            WriteToTxt(listBoxCategoriesSelectedList(index));
+        }
 
-		private BindingList<Product> listBoxCategoriesSelectedList(int index)
-		{
-			if (listBoxCategories.SelectedIndex != 0)
-			{
-				BindingList<Product> catogoryResultsDisplayed = CategoryHelpingMethod.SelectedCategoryInstances(ProductList, _Categories[listBoxCategories.SelectedIndex]);
-				dataGridView1.DataSource = catogoryResultsDisplayed;
-				return catogoryResultsDisplayed;
-			}
-			else
-			{
-				dataGridView1.DataSource = ProductList;
-				return ProductList;
-			}
-		}
+        private BindingList<Product> listBoxCategoriesSelectedList(int index)
+        {
+            if (listBoxCategories.SelectedIndex != 0)
+            {
+                BindingList<Product> catogoryResultsDisplayed = CategoryHelpingMethod.SelectedCategoryInstances(ProductList, _Categories[listBoxCategories.SelectedIndex]);
+                dataGridView1.DataSource = catogoryResultsDisplayed;
+                return catogoryResultsDisplayed;
+            }
+            else
+            {
+                dataGridView1.DataSource = ProductList;
+                return ProductList;
+            }
+        }
 
-		private void WriteToTxt(BindingList<Product> listOfProducts)
-		{
-			string txtContent = "";
+        private void WriteToTxt(BindingList<Product> listOfProducts)
+        {
+            string txtContent = "";
 
-			foreach (var product in listOfProducts)
-			{
-				txtContent += $"Produktnavn: {product.ProductName}, " +
-							  $"Produktkategori: {product.Category}, " +
-							  $"Produktpris: {product.Price}, " +
-							  $"Lagerbeholdning: {product.Stock}\n";
-			}
+            foreach (var product in listOfProducts)
+            {
+                txtContent += $"Produktnavn: {product.ProductName}, " +
+                              $"Produktkategori: {product.Category}, " +
+                              $"Produktpris: {product.Price}, " +
+                              $"Lagerbeholdning: {product.Stock}\n";
+            }
 
-			SaveFileDialog saveFileDialog = new SaveFileDialog();
-			saveFileDialog.Filter = "Tekstfil|*.txt";
-			saveFileDialog.FileName = $"Lager over {_Categories[listBoxCategories.SelectedIndex]}.txt";
-			saveFileDialog.Title = "Gem din tekstfil";
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Tekstfil|*.txt";
+            saveFileDialog.FileName = $"Lager over {_Categories[listBoxCategories.SelectedIndex]}.txt";
+            saveFileDialog.Title = "Gem din tekstfil";
 
-			if (saveFileDialog.ShowDialog() == DialogResult.OK)
-			{
-				File.WriteAllText(saveFileDialog.FileName, txtContent);
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(saveFileDialog.FileName, txtContent);
 
-				MessageBox.Show($"Oprettelse af tekstfil succesfuld\n\n" +
-							$"Fil sti: {saveFileDialog.FileName}");
-			}
-		}
+                MessageBox.Show($"Oprettelse af tekstfil succesfuld\n\n" +
+                            $"Fil sti: {saveFileDialog.FileName}");
+            }
+        }
 
-		private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
-		{
-			WinShowProductDetails winShowProductDetails = new WinShowProductDetails(ProductID);
-			winShowProductDetails.Show();
-			this.Close();
-		}
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            WinShowProductDetails winShowProductDetails = new WinShowProductDetails(ProductID);
+            winShowProductDetails.Show();
+            this.Close();
+        }
 
-		private void buttonMainMenu_Click(object sender, EventArgs e)
-		{
-			MainMenu mainMenu = new MainMenu();
-			mainMenu.Show();
-			this.Close();
-		}
+        private void buttonMainMenu_Click(object sender, EventArgs e)
+        {
+            MainMenu mainMenu = new MainMenu();
+            mainMenu.Show();
+            this.Close();
+        }
 
-		private void button1_Click(object sender, EventArgs e)
-		{
-			WinShowProductDetails winShowProductDetails = new WinShowProductDetails(ProductID);
-			winShowProductDetails.Show();
-			this.Close();
-		}
-	}
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WinShowProductDetails winShowProductDetails = new WinShowProductDetails(ProductID);
+            winShowProductDetails.Show();
+            this.Close();
+        }
+    }
 }
